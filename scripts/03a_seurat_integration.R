f1977eb9733c6442798d84f03ab29aec28944268
Andrew Chen <andrewdchen@berkeley.edu>
2024-12-05 16:29:49 -0500

    remove unnecessary library packages

--- scripts/03a_seurat_integration.R
+++ scripts/03a_seurat_integration.R
@@ -1,12 +1,6 @@
-library(SummarizedExperiment)
 library(Seurat)
 library(SeuratData)
 library(SeuratWrappers)
-library(Azimuth)
-library(ggplot2)
-library(patchwork)
-library(dplyr)
-library(ggrepel)
 library(doFuture)
 # doFuture::registerDoFuture()
 # future::plan("multisession", workers = 4)
@@ -27,49 +21,48 @@ seurat_paths <- list(combined = list(read = file.path(PATH, "data/sc/combined_se
                                   save = file.path(PATH, "data/sc/combined_strom_subset_int.rds"))
                      )
 
-# seurat_paths <- list(combined = file.path(PATH, "data/combined_seurat_filtered.rds"))
 
 foreach(seurat_path_name = names(seurat_paths)) %do% {
   seurat_path <- seurat_paths[[seurat_path_name]]
   seurat_obj <- readRDS(seurat_path[["read"]])
   print(seurat_obj)
-  # # 1. Harmony
-  # seurat_obj <- IntegrateLayers(
-  #   object = seurat_obj, method = HarmonyIntegration,
-  #   orig.reduction = "pca", new.reduction = "harmony",
-  #   verbose = TRUE)
-  # print(paste0("Done with Harmony for ", seurat_path_name))
-  # seurat_obj <- RunUMAP(seurat_obj, reduction = "harmony", dims = 1:200, reduction.name = "umap.harmony")
-  # saveRDS(seurat_obj, seurat_path[["save"]])
-  # write.csv(seurat_obj@reductions$harmony@cell.embeddings,
-  #           file.path(PATH, paste0("data/embeddings/harmony_embedding_", seurat_path_name, ".csv")))
+  # 1. Harmony
+  seurat_obj <- IntegrateLayers(
+    object = seurat_obj, method = HarmonyIntegration,
+    orig.reduction = "pca", new.reduction = "harmony",
+    verbose = TRUE)
+  print(paste0("Done with Harmony for ", seurat_path_name))
+  seurat_obj <- RunUMAP(seurat_obj, reduction = "harmony", dims = 1:200, reduction.name = "umap.harmony")
+  saveRDS(seurat_obj, seurat_path[["save"]])
+  write.csv(seurat_obj@reductions$harmony@cell.embeddings,
+            file.path(PATH, paste0("data/embeddings/harmony_embedding_", seurat_path_name, ".csv")))
 
-  # # 2. fastMNN
-  # DefaultAssay(seurat_obj) <- "RNA"
-  # seurat_obj <- IntegrateLayers(
-  #   object = seurat_obj, method = FastMNNIntegration,
-  #   new.reduction = "integrated.mnn",
-  #   verbose = TRUE)
-  # print(paste0("Done with fastMNN for ", seurat_path_name))
-  # seurat_obj <- RunUMAP(seurat_obj, reduction = "integrated.mnn", dims = 1:50, reduction.name = "umap.mnn")
-  # saveRDS(seurat_obj, seurat_path[["save"]])
-  # write.csv(seurat_obj@reductions$integrated.mnn@cell.embeddings,
-  #           file.path(PATH, paste0("data/embeddings/mnn_embedding_", seurat_path_name, ".csv")))
-  
-  # # 3. RPCA
-  # # Using sctransform to get batch corrected counts through v5 RPCA
-  # seurat_obj <- SCTransform(seurat_obj)
-  # #seurat_obj <- RunPCA(seurat_obj, npcs = 200)
-  # print(seurat_obj)
-  # seurat_obj <- IntegrateLayers(
-  #   object = seurat_obj, method = RPCAIntegration,
-  #   orig.reduction = "pca", new.reduction = "integrated.rpca", normalization.method = "SCT", k.anchor = 20,
-  #   verbose = TRUE)
-  # print(paste0("Done with RPCA for ", seurat_path_name))
-  # seurat_obj <- RunUMAP(seurat_obj, reduction = "integrated.rpca", dims = 1:200, reduction.name = "umap.rpca")
-  # saveRDS(seurat_obj, seurat_path[["save"]])
-  # write.csv(seurat_obj@reductions$integrated.rpca@cell.embeddings,
-  #           file.path(PATH, paste0("data/embeddings/rpca_embedding_", seurat_path_name, ".csv")))
+  # 2. fastMNN
+  DefaultAssay(seurat_obj) <- "RNA"
+  seurat_obj <- IntegrateLayers(
+    object = seurat_obj, method = FastMNNIntegration,
+    new.reduction = "integrated.mnn",
+    verbose = TRUE)
+  print(paste0("Done with fastMNN for ", seurat_path_name))
+  seurat_obj <- RunUMAP(seurat_obj, reduction = "integrated.mnn", dims = 1:50, reduction.name = "umap.mnn")
+  saveRDS(seurat_obj, seurat_path[["save"]])
+  write.csv(seurat_obj@reductions$integrated.mnn@cell.embeddings,
+            file.path(PATH, paste0("data/embeddings/mnn_embedding_", seurat_path_name, ".csv")))
+
+  # 3. RPCA
+  # Using sctransform to get batch corrected counts through v5 RPCA
+  seurat_obj <- SCTransform(seurat_obj)
+  seurat_obj <- RunPCA(seurat_obj, npcs = 200)
+  print(seurat_obj)
+  seurat_obj <- IntegrateLayers(
+    object = seurat_obj, method = RPCAIntegration,
+    orig.reduction = "pca", new.reduction = "integrated.rpca", normalization.method = "SCT", k.anchor = 20,
+    verbose = TRUE)
+  print(paste0("Done with RPCA for ", seurat_path_name))
+  seurat_obj <- RunUMAP(seurat_obj, reduction = "integrated.rpca", dims = 1:200, reduction.name = "umap.rpca")
+  saveRDS(seurat_obj, seurat_path[["save"]])
+  write.csv(seurat_obj@reductions$integrated.rpca@cell.embeddings,
+            file.path(PATH, paste0("data/embeddings/rpca_embedding_", seurat_path_name, ".csv")))
 
 }
 
